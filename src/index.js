f395c2feb609b9fe717e2ad258d5a3a1fa8a5e00
Youssef Girgeis <youssefgirgeis@gmail.com>
2022-03-14 14:43:31 -0400

    handle sass files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 //const sayHello = require('./greeting');
 import { sayHello } from './greeting';
 import lib from './lib.css';
+import application from './application.scss';
 
 alert('hello world!');
 console.log('Welcome to Webpack 🎈🎈');
